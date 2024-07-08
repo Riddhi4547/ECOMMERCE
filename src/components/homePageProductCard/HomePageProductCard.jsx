ff59937fdb1235e43dd788455b523b6a2b0b0094
Riddhi4547 <kesharwaniriddhi80@gmail.com>
2024-07-08 15:53:59 +0530

    scroll

--- src/components/homePageProductCard/HomePageProductCard.jsx
+++ src/components/homePageProductCard/HomePageProductCard.jsx
@@ -1,4 +1,8 @@
 // productData 
+
+import { useNavigate } from "react-router-dom";
+
+
 const productData = [
     {
         id: 1,
@@ -75,6 +79,8 @@ const productData = [
 ]
 
 const HomePageProductCard = () => {
+    const navigate = useNavigate();
+
     return (
         <div className="mt-10">
             {/* Heading  */}
@@ -92,6 +98,7 @@ const HomePageProductCard = () => {
                                 <div key={index} className="p-4 w-full md:w-1/4">
                                     <div className="h-full border border-gray-300 rounded-xl overflow-hidden shadow-md cursor-pointer">
                                             <img
+                                            onClick={()=> navigate('/productinfo')}
                                                 className="lg:h-80  h-96 w-full"
                                                 src={image}
                                                 alt="blog"
