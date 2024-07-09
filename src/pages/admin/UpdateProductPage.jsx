16ae538f69121862dbd5542d65a0e6ea60d503f1
Riddhi4547 <kesharwaniriddhi80@gmail.com>
2024-07-09 21:26:13 +0530

    updatefinal

--- src/pages/admin/UpdateProductPage.jsx
+++ src/pages/admin/UpdateProductPage.jsx
@@ -1,6 +1,9 @@
-import { Timestamp } from "firebase/firestore";
-import { useState } from "react";
+import { doc, getDoc, setDoc, Timestamp } from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import myContext from "../../context/myContext";
+import { fireDB } from "../../firebase/FirebaseConfig";
+import toast from "react-hot-toast";
 
 const categoryList = [
     {
@@ -56,6 +59,54 @@ const UpdateProductPage = () => {
         )
     });
 
+    // Get Single Product Function
+    const getSingleProductFunction = async () => {
+        setLoading(true);
+        try {
+            const productTemp = await getDoc(doc(fireDB, "products", id))
+            //   console.log(product.data())
+            const product = productTemp.data();
+            setProduct({
+                title: product?.title,
+                price: product?.price,
+                productImageUrl: product?.productImageUrl,
+                category: product?.category,
+                description: product?.description,
+                time: product?.time,
+                date: product?.date
+            })
+            setLoading(false);
+
+        } catch (error) {
+            getSingleProductFunction();
+            console.log(error);
+            setLoading(false);
+        }
+    }
+
+    //update product
+    const updateProduct = async () => {
+        setLoading(true)
+        try {
+
+            await setDoc(doc(fireDB, 'products', id), product)
+            toast.success("Product Updated successfully")
+            getAllProductFunction();
+            setLoading(false)
+            navigate('/admin-dashboard')
+
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+
+
+    useEffect(() => {
+        getSingleProductFunction();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <div>
             <div className='flex justify-center items-center h-screen'>
@@ -74,6 +125,13 @@ const UpdateProductPage = () => {
                         <input
                             type="text"
                             name="title"
+                            value={product.title}
+                            onChange={(e) => {
+                                setProduct({
+                                    ...product,
+                                    title: e.target.value
+                                })
+                            }}
                             placeholder='Product Title'
                             className='bg-pink-50 border text-pink-300 border-pink-200 px-2 py-2 w-96 rounded-md outline-none placeholder-pink-300'
                         />
@@ -84,6 +142,13 @@ const UpdateProductPage = () => {
                         <input
                             type="number"
                             name="price"
+                            value={product.price}
+                            onChange={(e) => {
+                                setProduct({
+                                    ...product,
+                                    price: e.target.value
+                                })
+                            }}
                             placeholder='Product Price'
                             className='bg-pink-50 border text-pink-300 border-pink-200 px-2 py-2 w-96 rounded-md outline-none placeholder-pink-300'
                         />
@@ -94,6 +159,13 @@ const UpdateProductPage = () => {
                         <input
                             type="text"
                             name="productImageUrl"
+                            value={product.productImageUrl}
+                            onChange={(e) => {
+                                setProduct({
+                                    ...product,
+                                    productImageUrl: e.target.value
+                                })
+                            }}
                             placeholder='Product Image Url'
                             className='bg-pink-50 border text-pink-300 border-pink-200 px-2 py-2 w-96 rounded-md outline-none placeholder-pink-300'
                         />
@@ -102,6 +174,13 @@ const UpdateProductPage = () => {
                     {/* Input Four  */}
                     <div className="mb-3">
                         <select
+                            value={product.category}
+                            onChange={(e) => {
+                                setProduct({
+                                    ...product,
+                                    category: e.target.value
+                                })
+                            }}
                             className="w-full px-1 py-2 text-pink-300 bg-pink-50 border border-pink-200 rounded-md outline-none  ">
                             <option disabled>Select Product Category</option>
                             {categoryList.map((value, index) => {
@@ -112,10 +191,16 @@ const UpdateProductPage = () => {
                             })}
                         </select>
                     </div>
-
                     {/* Input Five  */}
                     <div className="mb-3">
                         <textarea
+                        value={product.description}
+                        onChange={(e) => {
+                            setProduct({
+                                ...product,
+                                description: e.target.value
+                            })
+                        }} 
                             name="description" placeholder="Product Description" rows="5" className=" w-full px-2 py-1 text-pink-300 bg-pink-50 border border-pink-200 rounded-md outline-none placeholder-pink-300 ">
                         </textarea>
                     </div>
@@ -123,6 +208,7 @@ const UpdateProductPage = () => {
                     {/* Update Product Button  */}
                     <div className="mb-3">
                         <button
+                        onClick={updateProduct}
                             type='button'
                             className='bg-pink-500 hover:bg-pink-600 w-full text-white text-center py-2 font-bold rounded-md '
                         >
