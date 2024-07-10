e3447352db7b2ed3dcc44af4a1ee67bd3e00bc44
Riddhi4547 <kesharwaniriddhi80@gmail.com>
2024-07-10 21:16:04 +0530

    userOnAdmin

--- src/context/myState.jsx
+++ src/context/myState.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 import MyContext from "./myContext"
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { collection, deleteDoc, doc, onSnapshot, orderBy, query } from "firebase/firestore";
 import { fireDB } from "../firebase/FirebaseConfig";
+import toast from "react-hot-toast";
 
 const MyState = ({children}) => {
     
@@ -58,11 +59,51 @@ const MyState = ({children}) => {
         setLoading(false);
     }
 }
+// Delete oder Function
+const orderDelete = async (id) => {
+    setLoading(true)
+    try {
+        await deleteDoc(doc(fireDB, 'order', id))
+        toast.success('Order Deleted successfully')
+        getAllOrderFunction();
+        setLoading(false)
+    } catch (error) {
+        console.log(error)
+        setLoading(false)
+    }
+}
+
+// user State 
+const [getAllUser, setGetAllUser] = useState([]);
+
+//get all user
+const getAllUserFunction = async () => {
+    setLoading(true);
+    try {
+        const q = query(
+            collection(fireDB, "user"),
+            orderBy('time')
+        );
+        const data = onSnapshot(q, (QuerySnapshot) => {
+            let userArray = [];
+            QuerySnapshot.forEach((doc) => {
+                userArray.push({ ...doc.data(), id: doc.id });
+            });
+            setGetAllUser(userArray);
+            setLoading(false);
+        });
+        return () => data;
+    } catch (error) {
+        console.log(error);
+        setLoading(false);
+    }
+}
 
 //console.log(getAllOrder);
     useEffect(() => {
     getAllProductFunction();
     getAllOrderFunction();
+    getAllUserFunction();
 }, []);
   return (
     <MyContext.Provider value={{
@@ -70,7 +111,9 @@ const MyState = ({children}) => {
       setLoading,
       getAllProduct,
       getAllProductFunction,
-      getAllOrder
+      getAllOrder,
+      orderDelete,
+      getAllUser
     }}>
       {children}
     </MyContext.Provider>
